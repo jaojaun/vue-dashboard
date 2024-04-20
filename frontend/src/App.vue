f39c945ddae82dd8ed2c57258a1cef78fd9b964c
jaojaun <contatojmendonca@gmail.com>
2024-04-20 13:41:23 -0300

    first commit dashboard for portfolli

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Componente/>
+    <p>o dado do app é: {{ testando }}</p>
+  </div>
+</template>
+
+<script>
+import Componente from './components/component.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Componente
+  },
+  data() {
+    return {
+      testando: "Teste"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/component.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'component',
+    data() {
+        return {
+            nome: "João",
+            profissao: "Programador"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
